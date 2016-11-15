8a2d950e62fba4eee8d9d094975ac28ab9743035
Kokamido <kokamidokokosian@gmail.com>
2016-11-15 16:36:25 +0300

    Welcome. Again

---
+++ Assets/Scripts/SpriteRenderController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class SpriteRenderController : MonoBehaviour {
+
+    public Sprite flag;
+    public Sprite svaston;
+    private Sprite current;
+	// Use this for initialization
+	void Start () {
+        current = flag;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        this.GetComponent<SpriteRenderer>().sprite = current;
+	}
+
+    void OnMouseEnter()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            ChangeSprite();
+
+        }
+
+    }
+    private void ChangeSprite()
+    {
+        if (current.Equals(flag))
+        {
+            current = svaston;
+        }
+        else
+        {
+            current = flag;
+        }
+    }
+
+    
+}
